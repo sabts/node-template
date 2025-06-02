83e9230e540c906eba15a1e17ee7d74ac75e4060
sabts <sabrinataraschi@gmail.com>
2025-06-02 11:59:48 +0200

    .

--- src/index.js
+++ src/index.js
@@ -12,8 +12,33 @@ app.get("/read", (req, res) => {
   fs.readFile(userFilePath, (error, data) => {
     if (error)
       return res.status(500).send("Error 500: error al leer el archivo");
+    else {
+      jsonData = [...newUser];
+      console.log(jsonData);
+    }
+    const jsonData = JSON.parse(data);
+    res.send(jsonData);
+  });
+});
+
+app.get("/write", (req, res) => {
+  fs.readFile(userFilePath, (error, data) => {
+    if (error)
+      return res.status(500).send("Error 500: error al leer el archivo");
 
     const jsonData = JSON.parse(data);
     res.send(jsonData);
+  });
+
+  const newUser = JSON.stringify({
+    userId: "e65e1490-c230-4043-80bc-ea32fee5f57c",
+    name: "Tay Swift",
+    email: "taytay@hotmail.com",
+  });
+
+  fs.writeFile(userFilePath, newUser, error => {
+    if (error)
+      return res.status(500).send("Error 500: error al leer el archivo");
+    res.end();
   });
 });
